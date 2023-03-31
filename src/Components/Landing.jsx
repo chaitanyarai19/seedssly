e0181a79525a9a8b0e3240bc12681f494c4f8f13
Manish Shambhoo Sah <56181606+Msah30012001@users.noreply.github.com>
2023-03-31 12:00:54 +0530

    Update Landing.jsx
    update url

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
                 <h1>Seedsly</h1>
                 <h4>Sow the seeds of success with seedlsy</h4>
                 <div className="wrapper">
-                    <a className='primary-button' href='https://wingzacademy.com/kissania/' target="_blank" >Buy Farming Products</a>
+                    <a className='primary-button' href='https://seedsly.bloodanytime.com/index.php' target="_blank" >Buy Farming Products</a>
                 </div>
             </div>
         </div>
